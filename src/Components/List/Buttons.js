3680103800268174e3aaf23944126c735c031408
vaqsi1990 <vaqsii23@gmail.com>
2025-04-28 10:57:16 +0400

    why shecvla gverdis stilis

--- src/Components/List/Buttons.js
+++ src/Components/List/Buttons.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Item from './items';
 
 function Buttons({ open, setOpen }) {
-const handleCategoryChange = (item) => {
+const handleItemChange = (item) => {
     setOpen(item);
   };
 
@@ -11,12 +11,12 @@ const handleCategoryChange = (item) => {
     {Item.map((item) => (
         <button
           key={item.id}
-          onClick={() => handleCategoryChange(item)}
+          onClick={() => handleItemChange(item)}
           className={`outline-none rounded-lg cursor-pointer transition-all duration-150 ease-in-out 
             border border-solid border-black px-4 py-2 sm:px-6 sm:py-3 
             text-[14px] sm:text-[18px] uppercase focus:outline-none 
-            hover:bg-[#203e72] hover:text-white
-            ${open === item ? "bg-[#203e72] text-white" : "text-black"}`}
+            hover:bg-black hover:text-white
+            ${open === item ? "bg-black text-white" : "text-black"}`}
         >
           {item.text}
         </button>
