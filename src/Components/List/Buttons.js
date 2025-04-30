e4580168659733d26c236e3be77f5f0e99d55073
vaqsii <vaqsii23@gmail.com>
2025-04-30 22:07:53 +0400

    undone changes

--- src/Components/List/Buttons.js
+++ src/Components/List/Buttons.js
@@ -2,10 +2,8 @@ import React from 'react';
 import Item from './items';
 
 function Buttons({ open, setOpen }) {
-const handleItemChange = (item) => {
+const handleCategoryChange = (item) => {
     setOpen(item);
-    console.log(item);
-    
   };
 
   return (
@@ -13,12 +11,12 @@ const handleItemChange = (item) => {
     {Item.map((item) => (
         <button
           key={item.id}
-          onClick={() => handleItemChange(item)}
+          onClick={() => handleCategoryChange(item)}
           className={`outline-none rounded-lg cursor-pointer transition-all duration-150 ease-in-out 
             border border-solid border-black px-4 py-2 sm:px-6 sm:py-3 
             text-[14px] sm:text-[18px] uppercase focus:outline-none 
-            hover:bg-black hover:text-white
-            ${open === item ? "bg-black text-white" : "text-black"}`}
+            hover:bg-[#203e72] hover:text-white
+            ${open === item ? "bg-[#203e72] text-white" : "text-black"}`}
         >
           {item.text}
         </button>
--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -2,7 +2,7 @@
 import React, {  useState } from "react";
 import Buttons from "./Buttons";
 function List() {
-   const [open, setOpen] = useState("Home");
+   const [open, setOpen] = useState([]);
   return (
     <>
       <div className="flex flex-row items-center h-9 justify-center gap-8">
