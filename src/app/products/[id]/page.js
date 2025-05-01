9e9d783014429538e9be56c01da507f7357d3ebf
vaqsii <vaqsii23@gmail.com>
2025-05-01 22:46:50 +0400

    det

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -18,41 +18,47 @@ async function Page({params}) {
     // useEffect(() => {
     // if (id)  fetchProducts()
     // }, [id])
+
+   
+    
   return (
-    <main className="mt-5 text-white" >
-    <div className="container d-flex align-items-center flex-column justify-content-center min-vh-100">
-      <div className="row">
-        <div className="col-md-5">
-            <img
-              src={data.image}
-              alt={data.name}
-             
-            />
-        
-        </div>
-        <div className="col-md-7">
-          <div className="text-center">
-            <h1>{gameData?.name}</h1>
-            <h3> {gameData?.creator} </h3>
-            <h3> Price:{gameData?.price}$ </h3>
-            <hr />
-            <h3> {gameData?.description} </h3>
-          </div>
-          <div className='d-flex align-items-center'>
-            { user && (
-
-            <button onClick={handleAddToCart} type="button" className="btn btn-success">add to cart </button>
-            ) }
-          { user && user.isAdmin && (
-
-            <button onClick={deleteGame}  type="button" className="btn btn-success mx-3"> delete  </button>
-          )}
+    <>
+<div className="mt-16">
+  <div className="container">
+    <div className="mx-auto flex justify-between flex-col lg:flex-row gap-4 lg:gap-6">
+      
+   
+      <div className="w-full lg:w-[40rem]">
+        <div className="w-full">
+          <div className="flex flex-col md:grid md:grid-cols-[1fr_auto] gap-2 items-start">
+            <div className="relative w-full aspect-[4/3] rounded-xl overflow-hidden">
+              <img
+                src={data.image}
+                alt={data.name}
+                className="object-cover w-full h-full rounded-xl"
+              />
+            </div>
           </div>
         </div>
       </div>
+
+      <div className="w-full lg:max-w-[42%]">
+        <div className="pb-4 sm:pb-6 ">
+          <h2 className=" pb-4 text-2xl leading-tight tracking-tight text-black">
+          სახელი:   {data.name}
+          </h2>
+        <p className="pb-4 text-2xl leading-tight">სქესი: {data.gender} </p>
+        <p className="pb-4 text-2xl leading-tight">რასა: {data.species} </p>
+        <p className="pb-4 text-2xl leading-tight">სტატუსი: {data.status} </p>
+        <p className="pb-4 text-2xl leading-tight">წარმოშობა: {data.origin.name} </p>
+        </div>
+      </div>
+
     </div>
- 
-  </main>
+  </div>
+</div>
+
+</>
   )
 }
 
