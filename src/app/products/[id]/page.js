bffbf81e081fe420ac917ffd69f8301d69e8bd40
vaqsii <vaqsii23@gmail.com>
2025-05-01 22:46:42 +0400

    detalebis gverdi

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -19,12 +19,40 @@ async function Page({params}) {
     // if (id)  fetchProducts()
     // }, [id])
   return (
-<div>
-      <h1>{data.name}</h1>
-      <img src={data.image} alt={data.name} />
-      <p>Status: {data.status}</p>
-      <p>Species: {data.species}</p>
+    <main className="mt-5 text-white" >
+    <div className="container d-flex align-items-center flex-column justify-content-center min-vh-100">
+      <div className="row">
+        <div className="col-md-5">
+            <img
+              src={data.image}
+              alt={data.name}
+             
+            />
+        
+        </div>
+        <div className="col-md-7">
+          <div className="text-center">
+            <h1>{gameData?.name}</h1>
+            <h3> {gameData?.creator} </h3>
+            <h3> Price:{gameData?.price}$ </h3>
+            <hr />
+            <h3> {gameData?.description} </h3>
+          </div>
+          <div className='d-flex align-items-center'>
+            { user && (
+
+            <button onClick={handleAddToCart} type="button" className="btn btn-success">add to cart </button>
+            ) }
+          { user && user.isAdmin && (
+
+            <button onClick={deleteGame}  type="button" className="btn btn-success mx-3"> delete  </button>
+          )}
+          </div>
+        </div>
+      </div>
     </div>
+ 
+  </main>
   )
 }
 
